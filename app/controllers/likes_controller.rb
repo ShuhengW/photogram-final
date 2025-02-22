2dd44abb5cdb48fe8ea85c17d79d3ef5f711a07c
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-22 19:36:25 +0000

    fix

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController< ApplicationController
+
+  def create
+    @like = Like.new
+    @like.fan_id = current_user.id
+    @like.photo_id = params.fetch("query_photo_id")
+
+    if @like.valid?
+      @like.save
+      redirect_to("/photos/#{@like.photo_id}", { :notice => "Like created successfully." })
+    else
+      redirect_to("/photos/#{@like.photo_id}", { :alert => "Unable to create like." })
+    end
+
+  end 
+end 
