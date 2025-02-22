40417e2dbb7213dba996a145a7d4a25d9c53d5d7
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-22 04:31:36 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController< ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [:index] })
   def index
     @all_users = User.all
     render({ :template => "users/index" })
