e6d2f52f8405a9439c13c3491a486830d09660b5
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-22 06:08:16 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,29 @@ class UsersController< ApplicationController
       redirect_to("/users", { :alert => "User not found." })
     end
   end
+
+  def create_follow_request
+    @follow_request = FollowRequest.new
+    @follow_request.sender_id = current_user.id
+    @follow_request.recipient_id = params.fetch("query_recipient_id")
+
+    recipient = User.where({:id => params.fetch("query_recipient_id")}).at(0)
+
+    if recipient.private == true
+      @follow_request.status = "pending"
+    else
+      @follow_request.status = "accepted"
+    end
+
+    if @follow_request.valid?
+      @follow_request.save
+      if recipient&.private?
+        redirect_to("/", { :notice => "You're not authorized for that." })
+      else
+        redirect_to("/users/#{current_user.username}", { :notice => "Follow request sent." })
+      end
+    else
+      redirect_to("/users/#{current_user.username}", { :alert => "Unable to send follow request." })
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index" })
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
+  post("/insert_follow_request", { :controller => "users", :action => "create_follow_request" })
 
 end
